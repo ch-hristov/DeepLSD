429ce4bfacd2482a68967d77bf51ce26c1c05153
Christo S. Christov <ch-hristov@users.noreply.github.com>
2023-12-03 15:46:20 -0500

    Update homography_adaptation_df.py

--- deeplsd/scripts/homography_adaptation_df.py
+++ deeplsd/scripts/homography_adaptation_df.py
@@ -221,7 +221,6 @@ def ha_df_with_lines(img, lines, num = 100, border_margin=3, min_counts=5):
 
     return avg_df, avg_angle, avg_closest[:, :, [1, 0]], bg_mask # return the aggregated distance function maps, angle maps, closest point maps, and background mask
 
-
 def process_image(img_path, randomize_contrast, num_H, output_folder, lines):
     img = cv2.imread(img_path, 0)
     
@@ -231,19 +230,22 @@ def process_image(img_path, randomize_contrast, num_H, output_folder, lines):
     # Run homography adaptation
     df, angle, closest, bg_mask = ha_df_with_lines(img,lines= lines,num=num_H)
 
-    try:
-      # Save the DF in a hdf5 file
-      out_path = os.path.splitext(os.path.basename(img_path))[0]
-      out_path = os.path.join(output_folder, out_path) + '.hdf5'
-
-      print("Saving image file to " + out_path)
-      with h5py.File(out_path, "w") as f:
-          f.create_dataset("df", data=df.flatten())
-          f.create_dataset("line_level", data=angle.flatten())
-          f.create_dataset("closest", data=closest.flatten())
-          f.create_dataset("bg_mask", data=bg_mask.flatten())
-    except Exception as ex:
-      print(str(ex))
+    # Check if the hdf5 file already exists in the output folder
+    out_path = os.path.splitext(os.path.basename(img_path))[0]
+    out_path = os.path.join(output_folder, out_path) + '.hdf5'
+    if not os.path.exists(out_path):
+        try:
+          # Save the DF in a hdf5 file
+          print("Saving image file to " + out_path)
+          with h5py.File(out_path, "w") as f:
+              f.create_dataset("df", data=df.flatten())
+              f.create_dataset("line_level", data=angle.flatten())
+              f.create_dataset("closest", data=closest.flatten())
+              f.create_dataset("bg_mask", data=bg_mask.flatten())
+        except Exception as ex:
+          print(str(ex))
+    else:
+        print("The hdf5 file already exists. Skipping this image.")
 
 def export_ha(images_list, output_folder, lines_annotation_folder, num_H=100,
               rdm_contrast=False, n_jobs=1):
