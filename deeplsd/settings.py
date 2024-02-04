e639a55d14423d8478d0ec851c52b4eea59b0fd0
Christo S. Christov <you@example.com>
2024-02-04 10:58:11 -0500

    Change default path

--- deeplsd/settings.py
+++ deeplsd/settings.py
@@ -2,5 +2,5 @@
     Modify `EXPER_PATH` and `DATA_PATH` to point to any directories,
     with absolute or relative paths.
 """
-EXPER_PATH = '/content/drive/MyDrive/real-life-images'
-DATA_PATH = '//content/drive/MyDrive/deeplsd-lines-built'
+EXPER_PATH = '../experiments'
+DATA_PATH = '../lines'
