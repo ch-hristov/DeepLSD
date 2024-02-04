c8a7d206d40db2c8aa6046f4c0ddb34eee0cc6d0
Christo S. Christov <ch-hristov@users.noreply.github.com>
2024-02-04 10:51:25 -0500

    Create download.py

---
+++ download.py
@@ -0,0 +1,5 @@
+import gdown
+
+gdown.download('https://drive.google.com/uc?id=1NMrw_KkYUyocEiVAZyQi14vQcCj1ImFw')
+#gdown.download_folder('https://drive.google.com/uc?id=1LubadXKCNeWA28afEgvcjN5u00uKekL_')
+gdown.download_folder('https://drive.google.com/drive/folders/1LubadXKCNeWA28afEgvcjN5u00uKekL_', remaining_ok=True)
